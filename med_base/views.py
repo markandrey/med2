a1b3d5751779a21753497cba6459a117fff60919
Mark Andrey <markandreym@gmail.com>
2025-06-06 06:21:00 +0300

    first commit

---
+++ med_base/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('<h2>Главная</h2>')
+
+
+def about(request, name, age):
+    return HttpResponse(f'''
+        <h2>О пользователе</h2>
+        <p>Имя: {name}</p>
+        <p>Возраст: {age}</p>
+    ''')
+
+
+def contact(request):
+    return HttpResponse('<h2>Контакты</h2>')
